d3b0526bfe5860169e71128bdd2178e50dd0874a
Anton Raafat <138820743+tonyRaafat@users.noreply.github.com>
2025-02-12 15:27:54 +0200

    fix: appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { SentryGlobalFilter, SentryModule } from '@sentry/nestjs/setup';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerInerceptor } from './interceptors/logger.interceptor';
@@ -15,7 +14,6 @@ import { LoggerInerceptor } from './interceptors/logger.interceptor';
     MongooseModule.forRoot(`${process.env.MONGODB_URI}`),
     SentryModule.forRoot(),
     UsersModule,
-    ProductsModule,
   ],
   controllers: [AppController],
   providers: [
